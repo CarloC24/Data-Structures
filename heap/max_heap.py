c4855d4a6c89943ebaada46512f762fa6eab3a86
CarloC24 <carlo.clamucha@gmail.com>
2019-04-04 17:30:48 -0500

    added lru cache and partially completed max heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,59 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
+        self.size = 0
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        self.storage.append(value)
+        n = len(self.storage) - 1
+        for node in range(n//2, -1, -1):
+            self._sift_down(node)
+        return
 
-  def delete(self):
-    pass
+    def delete(self):
+        n = len(self.storage)
+        self.swap(0, n)
+        max = self.storage.pop(0)
+        self._bubble_up(n - 2)   # furthest left node
+        return max
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        maxVal = 0
+        for i in self.storage:
+            if i > maxVal:
+                maxVal = i
+        return maxVal
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        # length = len(self.storage)
+        # return length
+        pass
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        parent = (index - 1)//2
+        if self.storage[parent] < self.storage[index]:
+            self.swap(index, parent)
+    # base case; we've reached the top of the heap
+        if parent <= 0:
+            return
+        else:
+            self.bubble_up(parent)
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        child = 2 * index + 1
+        # base case, stop recursing when we hit the end of the heap
+        if child > len(self.storage) - 1:
+            return
+        # check that second child exists; if so find max
+        if (child + 1 <= len(self.storage) - 1) and (self.storage[child+1] > self.storage[child]):
+            child += 1
+        # preserves heap structure
+        if self.storage[index] < self.storage[child]:
+            self.swap(index, child)
+            self._sift_down(child)
+        else:
+            return
+
+    def swap(self, index, child):
+        self.storage[index], self.storage[child] = self.storage[child], self.storage[index]
+        return
--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,26 +1,23 @@
+import collections
+
+
 class LRUCache:
-  def __init__(self, limit=10):
-    pass
+    def __init__(self, limit=10):
+        self.limit = limit
+        self.cache = collections.OrderedDict()
 
-  """
-  Retrieves the value associated with the given key. Also
-  needs to move the key-value pair to the top of the order
-  such that the pair is considered most-recently used.
-  Returns the value associated with the key or None if the
-  key-value pair doesn't exist in the cache. 
-  """
-  def get(self, key):
-    pass
+    def get(self, key):
+        try:
+            value = self.cache.pop(key)
+            self.cache[key] = value
+            return value
+        except KeyError:
+            return None
 
-  """
-  Adds the given key-value pair to the cache. The newly-
-  added pair should be considered the most-recently used
-  entry in the cache. If the cache is already at max capacity
-  before this entry is added, then the oldest entry in the
-  cache needs to be removed to make room. Additionally, in the
-  case that the key already exists in the cache, we simply 
-  want to overwrite the old value associated with the key with
-  the newly-specified value. 
-  """
-  def set(self, key, value):
-    pass
+    def set(self, key, value):
+        try:
+            self.cache.pop(key)
+        except KeyError:
+            if len(self.cache) >= self.limit:
+                self.cache.popitem(last=False)
+        self.cache[key] = value
